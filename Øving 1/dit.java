d674aa799518d44e3b2c2dff84dafa5a9291773e
dan <daniel.skjortorp@gmail.com>
2025-10-11 11:01:54 +0200

    fikse 1

--- Øving 1/dit.java
+++ Øving 1/dit.java
@@ -1,12 +1,19 @@
-import static javax.swing.JOptionPane.*;
+import java.util.Scanner;
 public class dit {
 
     public static void main(String[]args) {
-        String sekunderLest = showInputDialog("Sekunder (s):");
-        double sekunder = Double.parseDouble(sekunderLest);
-        double endring = 60;
+        Scanner sc = new Scanner(System.in);
+        int sekunder = sc.nextInt();
+        int endring = 60;
+        int restmin = (sekunder/endring)%endring;
+        int restsek = sekunder%endring;
+        int timer   = (sekunder/endring)/endring;
+       
         
         
-        System.out.println(antall);
-    }
+        System.out.println("Det er " + restsek+ "sekunder");
+        System.out.println("Det er " + restmin+ "minutter");
+        System.out.println("Det er " + timer + "timer" );
+        sc.close();
+    }   
 }
