995b486dfaac00f18223294f65cb8ca9d3965b42
dan <daniel.skjortorp@gmail.com>
2025-10-11 13:26:26 +0200

    nærmere

--- Øving 7/Oppgave 2/tekst.java
+++ Øving 7/Oppgave 2/tekst.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class tekst {
     public Scanner sc = new Scanner(System.in);
     public String ting;
+    public int resu;
     
     public tekst(String ting){
         this.ting = ting;
@@ -14,5 +15,35 @@ public class tekst {
         int lengde = me.length;
         System.out.println(lengde);
         }
+    public void gjen(){
+        String regex = " ";
+        String[] me = ting.split(regex);
+        double lengde = me.length;
+        for(int i = 0; i < lengde; i++){
+            String l = me[i];
+            resu += l.length();
+        }
+        double snitt = resu /lengde;
+        System.out.printf("%.2f%n",snitt);
+    }
+    public void fleregjen(){
+        String regex = " ";
+        String[] me = ting.split(regex);
+        double lengde = me.length;
+        for(int i = 0; i < lengde; i++){
+            String l = me[i];
+            resu += l.length();
+        }
+        double snitt = resu /lengde;
+        System.out.printf("%.2f%n",snitt);
+        System.out.println();
+    }
+    public void normal(){
+        System.out.println(ting);
+    }
+    public void upper(){
+        String upper = ting.toUpperCase();
+        System.out.println(upper);
+    }
     }
 
--- Øving 7/Oppgave 2/test_tekst.java
+++ Øving 7/Oppgave 2/test_tekst.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class test_tekst {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String ting = sc.nextLine().toLowerCase();
+        String ting = sc.nextLine();
         tekst obj = new tekst(ting);
 
         obj.antall();
+        obj.gjen();
+        //obj.fleregjen();
+        obj.normal();
+        obj.upper();
 
         sc.close();
     
