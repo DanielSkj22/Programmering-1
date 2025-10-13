163a20da41da509c85f1dcb1ae97ba13bbaae533
dan <daniel.skjortorp@gmail.com>
2025-10-13 17:11:40 +0200

    fikling

---
+++ Øving 8/Oppgave 1/person_test.java
@@ -0,0 +1,11 @@
+public class person_test {
+    public static void main(String[] args) {
+        Person obj = new Person();
+        obj.setName("Daniel");
+        obj.setName2("Skjørtorp");
+        obj.setÅr("2002");
+        System.out.println(obj.getName());
+        System.out.println(obj.getName1());
+        System.out.println(obj.getÅr());
+    }
+}
