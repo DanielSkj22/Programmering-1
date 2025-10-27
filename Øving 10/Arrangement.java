4bbca89d889cbb63fe313d20bea4faefdc9dce9a
dan <daniel.skjortorp@gmail.com>
2025-10-27 11:46:18 +0100

    rydding

--- Øving 10/Arrangement.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public class Arrangement{
-    public ArrayList<String> konsert = new ArrayList<String>();
-}
--- Øving 10/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-    Arrangement obj = new Arrangement();
-
-
-    System.out.println(obj.konsert);
-    }
-}
