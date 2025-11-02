a51f7bbe43d7054f127c46b26ba5b37a232d4cb6
dan <daniel.skjortorp@gmail.com>
2025-11-02 16:13:31 +0100

    rydding

--- Øving 10/Oppgave 1/test.java
+++ Øving 10/Oppgave 1/test.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class test {
-    
+    public static void main(String[] args) {
+        ArrayList<String> menyer = new ArrayList<>();
+        ArrayList<String> retter = new ArrayList<>();
+
+        retter.add("fisk");
+        retter.add("suppe");
+        retter.add("Hovedrett");
+        retter.add("143");
+
+        menyer.toString();
+        System.out.println(menyer);
+        System.out.println(menyer.get(0));
+    }
 }
